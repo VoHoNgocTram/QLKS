1dc8df6e32fdcd23b9729bc2c2fb9f4947ddb4e3
VoHoNgocTram <114790013+VoHoNgocTram@users.noreply.github.com>
2022-10-13 12:36:26 +0700

    Add files via upload

---
+++ QLKS/NhanVien.java
@@ -0,0 +1,72 @@
+/*package QLKS;
+import java.util.Scanner;
+public class NhanVien{
+    private String name;
+    private String ID;  
+    private String phoneNumber;  
+    
+
+        public NhanVien(String name,  String ID, String phoneNumber){
+            super();
+            this.name=name;
+            this.ID=ID;
+            this.phoneNumber=phoneNumber;
+        }
+
+        
+        public NhanVien() {
+        }
+
+
+        public String getName() {
+        return name;
+        }
+
+        public void setName(String name) {
+        this.name = name;
+        }
+        
+        public String getID() {
+            return ID;
+        }
+
+        public void setMSNV(String ID) {
+            this.ID=ID;
+        }
+        
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        @Override
+        public String toString() {
+        return "Customer [name=" + name + ", ID=" + ID + ", phoneNumber=" + phoneNumber +"]";
+        }
+
+        public void nhapNV(){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Thong tin nhan vien : ");
+        System.out.print("\n Name: ");
+        name=in.nextLine();
+        System.out.print("\n ID : ");
+        ID=in.nextLine();
+        System.out.print("\n Phone Number: ");
+        phoneNumber=in.nextLine();
+        in.close();
+        }
+
+        public void xuatNV()
+        {
+            System.out.println("Name : " + name);
+            System.out.println("ID : " + ID);
+            System.out.println("PhoneNumber : " + phoneNumber);
+        }
+        
+}*/
+
+
